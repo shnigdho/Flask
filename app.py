08574f144a2333c750e6478e3076813914a6b128
Shnigdho <ah.sabbir108677@gmail.com>
2025-02-12 16:47:08 +0600

    done

--- app.py
+++ app.py
@@ -5,6 +5,48 @@ app = Flask(__name__)
 
 
 
+ENV ='dev'
+   
+if ENV == 'dev':
+    
+     app.debug = True
+     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgresql:sabbir@localhost:5432/Berger'
+
+else:
+    app.debug == False
+    app.confiq['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+
+db = SQLAlchemy(app)
+
+
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    mobile = db.Column(db.Integer)
+    HRN = db.Column(db.Integer)
+
+
+    def __init__(self, customer, mobile, HRN):
+        self.customer = customer
+        self.mobile = mobile
+        self.HRN = HRN
+
+
+
+
+    # def __init__(self, customer_name, customer_mobile_number, customer_HRN_number):
+    #     self.customer_name = customer_name
+    #     self.customer_mobile_number = customer_mobile_number
+    #     self.customer_HRN_number = customer_HRN_number
+
+
+
+
    
 
 
@@ -19,10 +61,10 @@ def index():
 def submit():
     if request.method == 'POST':
         customer = request.form['customer']
-        customer_mobile_Number = request.form['customer_1']
-        customer_HRN_Number = request.form['customer_2']
+        mobile = request.form['mobile']
+        HRN = request.form['HRN']
         # print(customer, customer_mobile_Number, customer_HRN_Number)
-        if customer == '' or customer_mobile_Number == '' or customer_HRN_Number == '':
+        if customer == '' or mobile == '' or HRN == '':
             return render_template('index.html', message='Please fill all the fields')
         return render_template('success.html')
         
